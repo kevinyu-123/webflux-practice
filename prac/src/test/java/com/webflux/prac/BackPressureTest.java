31d1f9defa09a21fd22d62b8442f2296688cf318
hj <tntj123@naver.com>
2023-03-01 21:58:35 +0900

    backpressure begin

---
+++ prac/src/test/java/com/webflux/prac/BackPressureTest.java
@@ -0,0 +1,47 @@
+package com.webflux.prac;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class BackPressureTest {
+
+    @Test
+    public void testBackPressure(){
+        var numbers = Flux.range(1,10).log();
+//        numbers.subscribe(s -> {
+//            System.out.println("num : "+numbers);
+        numbers.subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(3);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("value : " + value);
+                if(value == 3 ){
+                    cancel();
+                }
+
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                super.hookOnComplete();
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                super.hookOnError(throwable);
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                super.hookOnCancel();
+            }
+        });
+
+    }
+}
