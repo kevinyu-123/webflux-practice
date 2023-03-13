4dceef305b08a524f9f7762ee289b26b50fb0d7e
hj <tntj123@naver.com>
2023-03-13 23:51:32 +0900

    back pressure buffer / drop

--- prac/src/test/java/com/webflux/prac/BackPressureTest.java
+++ prac/src/test/java/com/webflux/prac/BackPressureTest.java
@@ -44,4 +44,86 @@ public class BackPressureTest {
         });
 
     }
+
+    @Test
+    public void testBackPressureDrop(){
+        var numbers = Flux.range(1,10).log();
+//        numbers.subscribe(s -> {
+//            System.out.println("num : "+numbers);
+        numbers
+                .onBackpressureDrop(integer -> {
+                    System.out.println("Dropped value : "+ integer);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(3);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("value : " + value);
+                if(value == 3 ){
+                    hookOnCancel();
+                }
+
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                super.hookOnComplete();
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                super.hookOnError(throwable);
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                super.hookOnCancel();
+            }
+        });
+
+    }
+
+    @Test
+    public void testBackPressureBuffer(){
+        var numbers = Flux.range(1,10).log();
+//        numbers.subscribe(s -> {
+//            System.out.println("num : "+numbers);
+        numbers
+                .onBackpressureBuffer(10, integer -> System.out.println("buffered value : " + integer))
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(3);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        System.out.println("value : " + value);
+                        if(value == 3 ){
+                            hookOnCancel();
+                        }
+
+                    }
+
+                    @Override
+                    protected void hookOnComplete() {
+                        super.hookOnComplete();
+                    }
+
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        super.hookOnError(throwable);
+                    }
+
+                    @Override
+                    protected void hookOnCancel() {
+                        super.hookOnCancel();
+                    }
+                });
+
+    }
 }
